9a1ea919706b756a2c0d35a7fe6b30ebe0503f4e
Sebastian Morales <fono.smc@gmail.com>
2022-07-11 23:07:24 -0400

    Implementada la búsqueda de productos

---
+++ js/search.js
@@ -0,0 +1,96 @@
+const fragment = document.createDocumentFragment()
+const templateCard = document.getElementById('template-card').content
+const cards = document.getElementById('cards')
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchData()
+     if(localStorage.getItem('cart')){
+        cart = JSON.parse(localStorage.getItem('cart'))
+}})
+
+let cart = JSON.parse(localStorage.getItem('cart'));
+
+cards.addEventListener('click', e =>{
+    addCart(e)
+})
+
+const fetchData = async () => {
+    try {
+        let data = JSON.parse(localStorage.getItem('search'));
+
+        let verify = 0
+        
+        data.forEach( e => {
+            verify += 1 
+        })
+
+        if (verify !== 0){
+            showProducts(data);
+        } else{
+            document.getElementById('empty').innerHTML = `No se han encontrado productos`;
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const img = document.createElement("img");
+img.src = "../images/unknown.png";
+
+const showProducts = data => {
+    data.forEach(product=>{
+        if (product.url_image === null){
+            templateCard.querySelector('img').setAttribute('src', img.src)
+        }else if (product.url_image === ''){
+            templateCard.querySelector('img').setAttribute('src', img.src)
+        }else{
+            templateCard.querySelector('img').setAttribute('src', product.url_image)
+        }
+
+        templateCard.querySelector('h5').textContent = product.name
+        templateCard.querySelector('p').textContent = product.price
+        
+    if(product.discount !== 0 && product.discount !== null){
+            templateCard.querySelector('#discount').textContent = product.discount
+            templateCard.querySelector('#discountLine').style.visibility = '' 
+        } else{  
+            templateCard.querySelector('#discount').textContent = product.discount
+            templateCard.querySelector('#discountLine').style.visibility = 'hidden' 
+        }  
+        
+        templateCard.querySelector('.btn-outline-dark').dataset.id = product.id
+        
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+
+    cards.appendChild(fragment)
+}
+
+ const addCart = e =>{
+    if (e.target.classList.contains('btn-outline-dark')){
+      setCart(e.target.parentElement)
+      alert('Agregado al carrito');
+    }
+    e.stopPropagation();
+}
+
+const setCart = object => {
+    const product = {
+        id: object.querySelector('.btn-outline-dark').dataset.id,
+        name: object.querySelector('h5').textContent,
+        price: object.querySelector('p').textContent,
+        discount: object.querySelector('#discount').textContent,
+        amount: 1
+    }
+
+if(cart.hasOwnProperty(product.id)){
+    product.amount = cart[product.id].amount +1
+}
+
+cart[product.id] = {...product}
+
+localStorage.setItem('cart', JSON.stringify(cart))
+
+}
---
+++ js/searchProduct.js
@@ -0,0 +1,24 @@
+const buttonSearch = document.getElementById('searchButton')
+
+buttonSearch.addEventListener('click', event =>{
+    event.preventDefault();
+    searchProduct();
+})
+
+const searchProduct = async () => {
+
+    let search = document.getElementById('searchProduct').value;
+    if(search === ''){
+        search = 404;
+    }
+
+    try {
+        const res = await fetch('http://localhost:4000/api/products/search?product='+ search);
+        const data = await res.json();
+        localStorage.setItem('search', JSON.stringify(data));
+        window.location.href = "../search.html";
+
+    } catch (error) {
+        console.log(error);
+    }
+}
